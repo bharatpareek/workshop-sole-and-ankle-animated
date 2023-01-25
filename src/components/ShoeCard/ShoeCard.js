3ab634bd6170f9a6e89edb2898a027e6ff1e839d
Bharat Pareek <bharat.pareek1991@gmail.com>
2023-01-25 20:55:35 +0530

    Solution - Exercise 1.

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -73,15 +73,28 @@ const Link = styled.a`
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+`;
 
 const ImageWrapper = styled.div`
-  position: relative;
+  border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
 `;
 
 const Image = styled.img`
   width: 100%;
-  border-radius: 16px 16px 4px 4px;
+  display: block;
+  transform-origin: 50% 50%;
+  transition: transform 500ms;
+  will-change: transform;
+  @media (hover: hover) and (prefers-reduced-motion: no-preference) {
+    a:hover &,
+    a:focus & {
+      transform: scale(1.1);
+      transition: transform 200ms;
+    }
+  }
 `;
 
 const Row = styled.div`
